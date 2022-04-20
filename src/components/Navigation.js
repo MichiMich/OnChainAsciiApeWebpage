42b8a9a42db46fc3d8f89c46dd116a95a05f347c
MichiMich <michimich054@gmail.com>
2022-04-20 21:39:47 +0200

    menu one button created
    navbar with one button working, needs some styling and overlay of all elements

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,19 +5,35 @@ import {
     Route,
     Link
 } from 'react-router-dom';
-import { Button } from "web3uikit";
-
+import { Button, ConnectButton } from "web3uikit";
 import { Raffle } from './Raffle.js';
 import { Home } from "./Home.js";
+import "./css/styleNavbar.css";
+import { useState } from 'react';
 
 export function Navigation() {
+
+    const [show, setshow] = useState(false);
+
     return (
         <Router>
+            <div className="navbar">
+                <h2>OnChainAsciiApes</h2>
+                <Button onClick={() => setshow(true)} text='Menu' />
 
-            <div className="App">
-                <Link to="/"> <Button text="Home" ></Button></Link>
-                <Link to="/raffle"> <Button text="Raffle" >Mobile</Button></Link>
             </div>
+            {show &&
+                <div style={{ height: "100%", width: "100%", background: "black", position: "fixed", top: "0px", left: "0px" }}>
+                    <h1 onClick={() => setshow(false)} style={{ position: "fixed", top: "0px", right: "20px", color: "white" }}>X</h1>
+                    <ul>
+                        <li ><Link to="/"><h2 style={{ color: "white" }} onClick={() => setshow(false)}>Home</h2></Link></li>
+                        <li><Link to="/raffle"><h2 style={{ color: "white" }} onClick={() => setshow(false)}>Raffle</h2> </Link></li>
+
+                    </ul>
+
+
+                </div>
+            }
             <Routes>
                 <Route exact path='/' element={< Home />}></Route>
                 <Route exact path='/raffle' element={< Raffle />}></Route>
